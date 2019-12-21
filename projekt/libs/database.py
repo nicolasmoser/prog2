a47664638c6400a89f34131fdc7eb3f817e83ea6
nicolasmoser <55879898+nicolasmoser@users.noreply.github.com>
2019-12-21 10:12:44 +0100

    Dateiname umbenannt - Teil 1

--- projekt/libs/database.py
+++ projekt/libs/database.py
@@ -1,9 +1,9 @@
 import json
 
-def telefonbuch_lesen():
+def database_lesen():
     data = {}
     try:
-        with open('telefonbuch.txt', "r") as open_file:
+        with open('database.txt', "r") as open_file:
             data = json.load(open_file)
     except:
         print("Error with file!")
@@ -11,11 +11,11 @@ def telefonbuch_lesen():
         return data
 
 def eintrag_speichern(name, stunden, tage, lohn):
-    telefonbuch = telefonbuch_lesen()
-    telefonbuch[name] = {"name": name, "hours": stunden, "days": tage, "salary": lohn}   
-    print(telefonbuch)
-    with open('telefonbuch.txt', "w", encoding="utf-8") as open_file:
-        json.dump(telefonbuch, open_file)
+    database = database_lesen()
+    database[name] = {"name": name, "hours": stunden, "days": tage, "salary": lohn}   
+    print(database)
+    with open('database.txt', "w", encoding="utf-8") as open_file:
+        json.dump(database, open_file)
 
 def eintrag_speichern_von_formular(form_request):
     print(form_request)
@@ -27,11 +27,11 @@ def eintrag_speichern_von_formular(form_request):
 
 
 def person_suchen(form_request):
-    telefonbuch = telefonbuch_lesen()
+    database = database_lesen()
     name = form_request.get('name')
 
-    if name in telefonbuch:
-        return {name: telefonbuch[name]}
+    if name in database:
+        return {name: database[name]}
 
     
 
--- projekt/main.py
+++ projekt/main.py
@@ -4,32 +4,32 @@ from flask import redirect
 from flask import request
 from flask import url_for
 
-from libs import telefonbuch
+from libs import database
 
-app = Flask("Telefonbuch")
+app = Flask("database")
 
 @app.route("/")
 @app.route("/index")
 def index():
-    telbuchdaten = telefonbuch.telefonbuch_lesen()
-    return render_template("telefonbuch.html", telbuch=telbuchdaten)
+    telbuchdaten = database.database_lesen()
+    return render_template("database.html", telbuch=telbuchdaten)
 
 
 @app.route("/search/<name>")
 @app.route("/search", methods=['GET', 'POST'])
 def search(name=None):
     if (request.method == 'POST'):
-        person_eintrag = telefonbuch.person_suchen(request.form)
+        person_eintrag = database.person_suchen(request.form)
         name = request.form.get('name')
         print(person_eintrag)
-        return render_template("funktion.html", value=person_eintrag[name])
+        return render_template("tracker.html", value=person_eintrag[name])
 
-    return render_template("search.html")
+    return render_template("index.html")
 
 @app.route("/add", methods=['GET', 'POST']) 
 def add():
     if (request.method == 'POST'):
-        telefonbuch.eintrag_speichern_von_formular(request.form)
+        database.eintrag_speichern_von_formular(request.form)
         return redirect("/")
 
     return render_template("add.html")
